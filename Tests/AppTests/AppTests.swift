66c05fdc06cb14a6b7fe2f568ae2d1f08022d4f3
tosh7 <zlia.6.lj.425@gmail.com>
2023-11-23 18:45:52 +0900

    add test case testNoPath

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -12,4 +12,15 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+
+    func testNoPath() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+
+        try app.test(.GET, "", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "It works!")
+        })
+    }
 }
